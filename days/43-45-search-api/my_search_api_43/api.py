d80b5c2a4002a04a10cba06ac7c947e2b20be16b
Plamen Milenkov <plamen.milenkov@sap.com>
2018-11-01 14:30:26 +0200

    Day 45

--- days/43-45-search-api/my_search_api_43/api.py
+++ days/43-45-search-api/my_search_api_43/api.py
@@ -1,10 +1,23 @@
-import requests
+from typing import List
 
-def search(keyword):
+import requests
+from collections import namedtuple
+
+Article = namedtuple('Article', 'category, id,'
+                                'url, title, description')
+
+def search(keyword: str) -> List[Article]:
     print(f'Looking for {keyword}...')
 
     url = f'http://search.talkpython.fm/api/search?q={keyword}'
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        print('Cannot connect. Please try again later.')
+        return []
     response.raise_for_status()
 
-    return response.json()['results']
+    articles = []
+    for article in response.json()['results']:
+        articles.append(Article(**article))
+    return articles
--- days/43-45-search-api/my_search_api_43/program.py
+++ days/43-45-search-api/my_search_api_43/program.py
@@ -1,5 +1,6 @@
 import api
 import logbook
+from webbrowser import open as web_open
 
 logbook.RotatingFileHandler('my_search_api', level=logbook.TRACE).push_application()
 logger = logbook.Logger("Main")
@@ -8,10 +9,18 @@ def main():
     logger.info('Taking user input...')
     user_input = input('Enter search criteria: ')
     logger.info(f'Searching for {user_input}')
-    result = api.search(user_input)
-    print(f'Found {len(result)} results')
-    for r in result:
-        print(f"Title: {r.get('title')}")
+    articles = api.search(user_input)
+    print(f'Found {len(articles)} articles')
+    id_to_article_dict = {}
+    for r in articles:
+        id_to_article_dict[r.id] = r
+        print(f"[{r.id}] {r.title}: {r.category}")
+
+    print("*"*20)
+
+    user_input = input('For which article do you need more information [q for quit]: ')
+    if user_input != 'q':
+        web_open("http://talkpython.fm" + id_to_article_dict[int(user_input)].url, new=2)
     logger.info('End of execution')
 
 if __name__ == '__main__':
